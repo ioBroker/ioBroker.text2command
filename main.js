162f2678adc49411f364934d36b0ef7b39e001fb
Ingo Fischer <github@fischer-ka.de>
2019-01-08 21:21:38 +0100

    fix for check in compact mode

--- main.js
+++ main.js
@@ -303,7 +303,7 @@ function main() {
 }
 
 // If started as allInOne mode => return function to create instance
-if (typeof module !== undefined && module.parent) {
+if (module.parent) {
     module.exports = startAdapter;
 } else {
     // or start the instance directly
