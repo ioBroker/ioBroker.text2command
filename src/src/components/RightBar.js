201d256192eb82543eb0ffd92cfd30ea7665396d
RamzinDenis <denis.ramzin@mail.ru>
2020-06-12 15:27:28 +0300

    continue working on input handlers issue

--- src/src/components/RightBar.js
+++ src/src/components/RightBar.js
@@ -1,217 +1,257 @@
-import React, { Component, useEffect } from "react";
-import I18n from "@iobroker/adapter-react/i18n";
-import { TextField, Switch, Typography } from "@material-ui/core";
-import DialogSelectID from "@iobroker/adapter-react/Dialogs/SelectID";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import FormControl from "@material-ui/core/FormControl";
-import PropTypes from "prop-types";
-import { v4 as uuid } from "uuid";
+import React, { Component, useEffect } from 'react';
+import I18n from '@iobroker/adapter-react/i18n';
+import { TextField, Switch, Typography } from '@material-ui/core';
+import DialogSelectID from '@iobroker/adapter-react/Dialogs/SelectID';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import PropTypes from 'prop-types';
 
 export default class RightBar extends Component {
-	defaultState = {
-		words: "Выберите правило",
-		name: "Выберите правило",
-		interupt: false,
-		editable: false,
-		args: [
-			{
-				name: "Параметр 1",
-			},
-			{
-				name: "Параметр 2",
-			},
-		],
-		ack: {
-			name: "",
-			default: `${I18n.t("Confirmation text")}`,
-		},
-	};
+    defaultState = {
+        words: 'Выберите правило',
+        name: 'Выберите правило',
+        interupt: false,
+        editable: false,
+        args: [
+            {
+                default: 'Параметр 1',
+            },
+            {
+                default: 'Параметр 2',
+            },
+        ],
+        ack: {
+            name: '',
+            default: `${I18n.t('Confirmation text')}`,
+        },
+    };
 
-	state = {
-		localRule: this.defaultState,
-		showDialog: false,
-	};
+    state = {
+        localRule: this.defaultState,
+        showDialog: false,
+    };
 
-	createInput = ({
-		value,
-		label,
-		onChange,
-		type,
-		onClick,
-		disabled = this.state.localRule === this.defaultState,
-		keywords,
-	}) => {
-		if (!value && !label && !keywords) return;
-		return type !== "checkbox" ? (
-			<TextField
-				label={label}
-				id="outlined-basic"
-				variant="outlined"
-				size="small"
-				disabled={disabled}
-				value={value}
-				onClick={onClick}
-				onChange={event =>
-					this.setState({ localRule: { words: event.target.value } })
-				}
-			/>
-		) : (
-			<FormControl>
-				<FormControlLabel
-					value={value}
-					label={label}
-					labelPlacement={"start"}
-					control={<Switch onClick={onChange} />}
-				/>
-			</FormControl>
-		);
-	};
+    componentDidUpdate(prevProps, prevState) {
+        if (
+            prevProps.selectedRule?.name !== this.props.selectedRule?.name ||
+            prevState.localRule?.name !== this.state.localRule?.name
+        ) {
+            this.setState({
+                localRule: {
+                    ...this.props.selectedRule,
+                    interupt: true,
+                },
+            });
+        }
+    }
 
-	componentDidUpdate(prevProps, prevState) {
-		if (
-			prevProps.selectedRule?.name !== this.props.selectedRule?.name ||
-			prevState.localRule?.name !== this.state.localRule?.name
-		) {
-			this.setState({
-				newOptionsData: this.createOptionsData(),
-				localRule: {
-					...this.props.selectedRule,
-					interupt: true,
-				},
-			});
-		}
-	}
+    createInput = ({
+        value,
+        label,
+        onChange,
+        type,
+        onClick,
+        disabled = this.state.localRule === this.defaultState,
+        keywords,
+        key,
+        onSwitchChange,
+    }) => {
+        if (!value && !label && !keywords) return;
+        return type !== 'checkbox' ? (
+            <TextField
+                label={label}
+                id="outlined-basic"
+                variant="outlined"
+                size="small"
+                disabled={disabled}
+                value={value}
+                onClick={onClick}
+                onChange={onChange}
+                key={key}
+            />
+        ) : (
+            <FormControl>
+                <FormControlLabel
+                    value={value}
+                    label={label}
+                    labelPlacement={'start'}
+                    control={<Switch onClick={onSwitchChange} />}
+                    key={key}
+                />
+            </FormControl>
+        );
+    };
 
-	createOptionsData = (state = this.state) => {
-		const {
-			localRule: { words, interupt, args, ack, editable },
-		} = state;
+    createOptionsData = (state = this.state) => {
+        const {
+            localRule: { words, interupt, args, ack, editable },
+        } = state;
 
-		console.log(this.state);
+        console.log(this.state);
 
-		const { t } = I18n;
-		const createInput = this.createInput;
+        const { t } = I18n;
+        const createInput = this.createInput;
 
-		const isKeyWordsDisabled = () => {
-			if (editable === "undefined") return false;
-			else if (editable === false) return true;
-		};
+        const isKeyWordsDisabled = () => {
+            if (editable === 'undefined') return false;
+            else if (editable === false) return true;
+        };
 
-		const handleTextInputChange = (event, name) => {
-			this.setState({
-				localRule: {
-					...this.state.localRule,
-					[name]: event.target.value,
-				},
-			});
-		};
+        const handleTextInputChange = (event, name) => {
+            this.setState({
+                localRule: {
+                    ...this.state.localRule,
+                    [name]: event.target.value,
+                },
+            });
+        };
 
-		return [
-			{
-				title: `${t("Keywords")}:`,
-				item: createInput({
-					value: this.state.localRule?.words,
-					onChange: event =>
-						this.setState({
-							localRule: {
-								["words"]: event.target.value,
-							},
-						}),
-					keywords: true,
-					disabled: isKeyWordsDisabled(),
-				}),
-				id: 1,
-			},
-			{
-				title: `${t("Interupt")}:`,
-				item: (
-					<Switch
-						size="medium"
-						color={"primary"}
-						checked={interupt}
-						onClick={this.handleSwitchChange}
-					/>
-				),
-				id: 2,
-			},
-			{
-				title: `${t("Param")}:`,
-				item: createInput({
-					value: args && args[0]?.default,
-					label: args && args[0]?.name,
-					type: args && args[0]?.type,
-					onClick: () => this.setState({ showDialog: true }),
-				}),
-				id: 3,
-			},
-			{
-				title: `${t("Param")}:`,
-				item: createInput({
-					value: args && args[1]?.default,
-					label: args && args[1]?.name,
-				}),
-				id: 4,
-			},
-			{
-				title: `${t("Confirmation text")}:`,
-				item: createInput({
-					value: ack && ack.default,
-					label: ack && ack.name,
-					type: ack && ack.type,
-				}),
-				id: 5,
-			},
-		];
-	};
+        return [
+            {
+                title: `${t('Keywords')}:`,
+                item: createInput({
+                    value: this.state.localRule?.words || '',
+                    onChange: event => handleTextInputChange(event, 'words'),
+                    keywords: true,
+                    disabled: isKeyWordsDisabled(),
+                    key: 'keywords',
+                }),
+                id: 1,
+            },
+            {
+                title: `${t('Interupt')}:`,
+                item: (
+                    <Switch
+                        size="medium"
+                        color={'primary'}
+                        checked={interupt}
+                        onClick={this.handleSwitchChange}
+                    />
+                ),
+                id: 2,
+            },
+            {
+                title: `${t('Param')}:`,
+                item: createInput({
+                    value: args && this.state.localRule.args[0]?.default,
+                    label: args && args[0]?.name,
+                    type: args && args[0]?.type,
+                    onClick: this.handleSetDialogClick.bind(this, args && args[0]),
+                    key: 'Param1',
+                    onSwitchChange: () => console.log('ok'),
+                }),
+                id: 3,
+            },
+            {
+                title: `${t('Param')}:`,
+                item: createInput({
+                    value: args && this.state.localRule.args[1]?.default,
+                    label: args && args[1]?.name,
+                    onChange: event =>
+                        this.setState({
+                            localRule: {
+                                ...this.state.localRule,
+                                args: this.state.localRule.args.map((arg, index) =>
+                                    index > 0
+                                        ? {
+                                              ...arg,
+                                              default: event.target.value,
+                                          }
+                                        : arg
+                                ),
+                            },
+                        }),
+                    key: 'Param2',
+                }),
+                id: 4,
+            },
+            {
+                title: `${t('Confirmation text')}:`,
+                item: createInput({
+                    value: ack && ack.default,
+                    label: ack && ack.name,
+                    type: ack && ack.type,
+                    key: 'Confirmation text',
+                }),
+                id: 5,
+            },
+        ];
+    };
 
-	handleSwitchChange = () => {
-		this.setState({
-			localRule: {
-				...this.state.localRule,
-				interupt: !this.state.localRule.interupt,
-			},
-		});
-	};
+    handleSwitchChange = () => {
+        this.setState({
+            localRule: {
+                ...this.state.localRule,
+                interupt: !this.state.localRule.interupt,
+            },
+        });
+    };
 
-	render() {
-		const {
-			localRule: { name },
-			newOptionsData,
-		} = this.state;
+    handleSetDialogClick = arg => {
+        if (arg.type === 'id') {
+            this.setState({
+                showDialog: true,
+            });
+        }
+    };
 
-		return (
-			<div className="right-bar">
-				<div className="right-bar__container">
-					<Typography variant="h4" align="center" gutterBottom={true}>
-						{name || "Выберите правило"}
-					</Typography>
-					{this.createOptionsData().map(({ title, item, id }) => {
-						if (!item) return null;
-						return (
-							<div className="custom-card" key={id}>
-								<Typography variant="h5" component="h2" align="left">
-									{title}
-								</Typography>
-								{item}
-							</div>
-						);
-					})}
-				</div>
+    handleDialogSelectIdSubmit = selected => {
+        this.setState({
+            localRule: {
+                ...this.state.localRule,
+                args: this.state.localRule.args.map((arg, index) =>
+                    !index
+                        ? {
+                              ...arg,
+                              default: selected,
+                          }
+                        : arg
+                ),
+            },
+        });
+    };
 
-				{this.state.showDialog && (
-					<DialogSelectID
-						connection={this.props.socket}
-						title={"Select ID"}
-						onClose={() => this.setState({ showDialog: false })}
-						onOk={() => this.setState({ showDialog: false })}
-					/>
-				)}
-			</div>
-		);
-	}
+    render() {
+        const {
+            localRule: { name },
+            newOptionsData,
+        } = this.state;
+
+        const handleSubmit = this.handleDialogSelectIdSubmit;
+
+        return (
+            <div className="right-bar">
+                <div className="right-bar__container">
+                    <Typography variant="h4" align="center" gutterBottom={true}>
+                        {name || 'Выберите правило'}
+                    </Typography>
+                    {this.createOptionsData().map(({ title, item, id }) => {
+                        if (!item) return null;
+                        return (
+                            <div className="custom-card" key={id}>
+                                <Typography variant="h5" component="h2" align="left">
+                                    {title}
+                                </Typography>
+                                {item}
+                            </div>
+                        );
+                    })}
+                </div>
+
+                {this.state.showDialog && (
+                    <DialogSelectID
+                        connection={this.props.socket}
+                        title={'Select ID'}
+                        onClose={() => this.setState({ showDialog: false })}
+                        onOk={handleSubmit}
+                    />
+                )}
+            </div>
+        );
+    }
 }
 
 RightBar.propTypes = {
-	selectedRule: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-	socket: PropTypes.object.isRequired,
+    selectedRule: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+    socket: PropTypes.object.isRequired,
 };
