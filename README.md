f7cd97277886478005b3dda4b1898b177829b03f
GermanBluefox <dogafox@gmail.com>
2022-02-21 14:17:09 +0100

    Fixed tests

--- README.md
+++ README.md
@@ -261,7 +261,7 @@ If activated so by every command (no matter if the request came via state or sen
 -->
 
 ## Changelog
-### 2.2.0 (2022-02-21)
+### __WORK IN PROGRESS__
 * (bluefox) Checked the existence of sayit instance before output
 * (bluefox) Added the decimal places settings to temperature  
 * (bluefox) Added the second object ID to user queries
--- test/testCommands.js
+++ test/testCommands.js
@@ -68,12 +68,65 @@ const adapter = {
             cb('not found');
         }
     },
+    getForeignObjectAsync: function (id) {
+        if (id === 'temperatureC') {
+            return Promise.resolve({
+                _id: 'temperatureC',
+                common: {
+                    role: 'value.temperature',
+                    unit: '°C'
+                }
+            });
+        } else if (id === 'temperatureF') {
+            return Promise.resolve({
+                _id: 'temperatureF',
+                common: {
+                    role: 'value.temperature',
+                    unit: '°F'
+                }
+            });
+        } else if (id === 'temperatureNone') {
+            return Promise.resolve({
+                _id: 'temperatureNone',
+                common: {
+                }
+            });
+        } else if (id === 'someSwitch') {
+            return Promise.resolve({
+                _id: 'someSwitch',
+                common: {
+                    name: 'some switch',
+                    unit: '%',
+                    type: 'boolean'
+                },
+                type: 'state'
+            });
+        } else if (id === 'someLevel') {
+            return Promise.resolve({
+                _id: 'someLevel',
+                common: {
+                    name: 'some Level',
+                    unit: '%',
+                    type: 'number'
+                },
+                type: 'state'
+            });
+        } else {
+            return Promise.reject('not found');
+        }
+    },
     setForeignState: function (id, value, cb) {
         writtenValue = value;
-        cb(null);
+        typeof cb === 'function' && cb(null);
+    },
+    setState: function (id, value, cb) {
+        typeof cb === 'function' && cb(null);
     },
     getForeignState: function (id, cb) {
         cb(null, {val: 15});
+    },
+    getForeignStateAsync: function (id, cb) {
+        return Promise.resolve({val: 15});
     }
 };
 let systemConfig = {
@@ -85,7 +138,7 @@ simpleControl.init(systemConfig, adapter);
 describe('Commands: Test time', function () {
     it('must return current time', function (done) {
         simpleControl.sayTime(null, null, null, null, function (text) {
-            if (debug) console.log('sayTime returned: ' + text);
+            debug && console.log('sayTime returned: ' + text);
             expect(text).to.be.ok;
             expect(text).has.length(5);
             expect(text[2]).is.equal(':');
@@ -99,7 +152,7 @@ describe('Commands: Test time', function () {
 describe('Commands: Test name', function () {
     it('must return no name in english', function (done) {
         simpleControl.sayName('en', null, null, null, function (text) {
-            if (debug) console.log('sayName(en) returned: ' + text);
+            debug && console.log('sayName(en) returned: ' + text);
             expect(text).to.be.ok;
             done();
         });
@@ -107,7 +160,7 @@ describe('Commands: Test name', function () {
 
     it('must return no name in german', function (done) {
         simpleControl.sayName('de', null, null, null, function (text) {
-            if (debug) console.log('sayName(de) returned: ' + text);
+            debug && console.log('sayName(de) returned: ' + text);
             expect(text).to.be.ok;
             done();
         });
@@ -115,7 +168,7 @@ describe('Commands: Test name', function () {
 
     it('must return no name in russian', function (done) {
         simpleControl.sayName('ru', null, null, null, function (text) {
-            if (debug) console.log('sayName(ru) returned: ' + text);
+            debug && console.log('sayName(ru) returned: ' + text);
             expect(text).to.be.ok;
             done();
         });
@@ -123,70 +176,68 @@ describe('Commands: Test name', function () {
 
     it('must return name', function (done) {
         simpleControl.sayName('en', null, null, '1/2/3', function (text) {
-            if (debug) console.log('sayName(1/2/3) returned: ' + text);
+            debug && console.log('sayName(1/2/3) returned: ' + text);
             expect(text).to.be.ok;
             expect(parseInt(text, 10)).to.be.above(0);
             expect(parseInt(text, 10)).to.be.below(4);
             done();
         });
     });
-
-
 });
 
 describe('Commands: Test temperature', function () {
     it('must say temperature in english and celsius', function (done) {
         simpleControl.sayTemperature('en', null, ['temperatureC'], '%s %u/a %s %u/b %s %u', function (text) {
-            if (debug) console.log('sayTemperature(en, C) returned: ' + text);
+            debug && console.log('sayTemperature(en, C) returned: ' + text);
             expect(text.indexOf('15 degrees')).to.be.least(0);
             done();
         });
     });
     it('must say temperature in german and celsius', function (done) {
         simpleControl.sayTemperature('de', null, ['temperatureC'], '%s %u/a %s %u/b %s %u', function (text) {
-            if (debug) console.log('sayTemperature(de, C) returned: ' + text);
+            debug && console.log('sayTemperature(de, C) returned: ' + text);
             expect(text.indexOf('15 Grad')).to.be.least(0);
             done();
         });
     });
     it('must say temperature in russian and celsius', function (done) {
         simpleControl.sayTemperature('ru', null, ['temperatureC'], '%s %u/a %s %u/b %s %u', function (text) {
-            if (debug) console.log('sayTemperature(ru, C) returned: ' + text);
+            debug && console.log('sayTemperature(ru, C) returned: ' + text);
             expect(text.indexOf('15 градусов')).to.be.least(0);
             done();
         });
     });
     it('must say temperature in english and fahrenheit', function (done) {
         simpleControl.sayTemperature('en', null, ['temperatureF'], '%s %u/a %s %u/b %s %u', function (text) {
-            if (debug) console.log('sayTemperature(en, F) returned: ' + text);
+            debug && console.log('sayTemperature(en, F) returned: ' + text);
             expect(text.indexOf('15 degrees')).to.be.least(0);
             done();
         });
     });
     it('must say temperature in german and fahrenheit', function (done) {
         simpleControl.sayTemperature('de', null, ['temperatureF'], '%s %u/a %s %u/b %s %u', function (text) {
-            if (debug) console.log('sayTemperature(de, F) returned: ' + text);
+            debug && console.log('sayTemperature(de, F) returned: ' + text);
             expect(text.indexOf('15 Grad')).to.be.least(0);
             done();
         });
     });
     it('must say temperature in russian and fahrenheit', function (done) {
         simpleControl.sayTemperature('ru', null, ['temperatureF'], '%s %u/a %s %u/b %s %u', function (text) {
-            if (debug) console.log('sayTemperature(ru, F) returned: ' + text);
+            debug && console.log('sayTemperature(ru, F) returned: ' + text);
             expect(text.indexOf('15 градусов')).to.be.least(0);
             done();
         });
     });
     it('must say temperature in english with default units', function (done) {
         simpleControl.sayTemperature('en', null, ['temperatureNone'], '%s %u/a %s %u/b %s %u', function (text) {
-            if (debug) console.log('sayTemperature(en, Default) returned: ' + text);
+            debug && console.log('sayTemperature(en, Default) returned: ' + text);
             expect(text.indexOf('15 degrees')).to.be.least(0);
             done();
         });
     });
     it('must say temperature dont know', function (done) {
         simpleControl.sayTemperature('en', null, ['temperatureNone1'], '%s %u/a %s %u/b %s %u', function (text) {
-            if (debug) console.log('sayTemperature(en, unknownId) returned: ' + text);
+            debug && console.log('sayTemperature(en, unknownId) returned: ' + text);
             expect(text.indexOf('degrees')).to.be.below(0);
             expect(text).to.be.ok;
             done();
@@ -194,16 +245,17 @@ describe('Commands: Test temperature', function () {
     });
     it('must replace template with the value', function (done) {
         simpleControl.sayTemperature('en', null, ['temperatureC'], '{system.adapter.text2command.alive} a {system.adapter.text2command.0.connected}', function (text) {
-            if (debug) console.log('sayTemperature(en, C) returned: ' + text);
+            debug && console.log('sayTemperature(en, C) returned: ' + text);
             expect(text).to.be.equal('15 a 15');
             done();
         });
     });
 });
+
 describe('Commands: Build answer', function () {
     it('must replace bindings with the value', function (done) {
         simpleControl.buildAnswer('en', null, null, '{a: system.adapter.text2command.alive; a * 2} a {system.adapter.text2command.0.connected;*(3)}', function (text) {
-            if (debug) console.log('Build answer(en) returned: ' + text);
+            debug && console.log('Build answer(en) returned: ' + text);
             expect(text).to.be.equal('30 a 45');
             done();
         });
@@ -212,25 +264,25 @@ describe('Commands: Build answer', function () {
 
 describe('Commands: Test userQuery', function () {
     it('must return temperature on query', function (done) {
-        simpleControl.userQuery('en', null, ['temperatureC'], '%s grad/a %s grad/b %s grad', function (text) {
-            if (debug) console.log('userQuery(temperatureC) returned: ' + text);
+        simpleControl.userQuery('en', null, ['temperatureC'], '%s grad/a %s grad/b %s grad', text => {
+            debug && console.log('userQuery(temperatureC) returned: ' + text);
             expect(text.indexOf('15 grad')).to.be.at.least(0);
             done();
         });
     });
 
-    it('must return temperature on query', function (done) {
-        simpleControl.userQuery('en', null, ['temperatureC'], '%s %u/a %s %u /b %s %u', function (text) {
-            if (debug) console.log('userQuery(temperatureC, %u) returned: ' + text);
+    it('must return temperature on query 1', function (done) {
+        simpleControl.userQuery('en', null, ['temperatureC'], '%s %u/a %s %u /b %s %u', text => {
+            debug && console.log('userQuery(temperatureC, %u) returned: ' + text);
             expect(text.indexOf('15 degrees')).to.be.at.least(0);
             done();
         });
     });
 
-    it('must return temperature on query', function (done) {
-        simpleControl.userQuery('en', null, ['unknown'], '%s %u/a %s %u /b %s %u', function (text) {
-            if (debug) console.log('userQuery(unknown) returned: ' + text);
-            expect(text).to.be.not.ok;
+    it('must return temperature on query 2', function (done) {
+        simpleControl.userQuery('en', null, ['unknown'], '%s %u/a %s %u /b %s %u', text => {
+            debug && console.log('userQuery(unknown) returned: ' + text);
+            expect(text).to.be.equal('Error. See logs.');
             done();
         });
     });
@@ -239,7 +291,7 @@ describe('Commands: Test userQuery', function () {
 describe('Commands: Test device control', function () {
     it('must control device with predefined value', function (done) {
         simpleControl.userDeviceControl('en', null, ['someSwitch', 'true'], 'Value %s written', function (text) {
-            if (debug) console.log('userDeviceControl(someSwitch) returned: ' + text);
+            debug && console.log('userDeviceControl(someSwitch) returned: ' + text);
             expect(writtenValue).to.be.equal(true);
             expect(text).to.be.ok;
             expect(text.indexOf('Value true written')).to.be.at.least(0);
@@ -249,7 +301,7 @@ describe('Commands: Test device control', function () {
 
     it('must control device with variable value 60.5', function (done) {
         simpleControl.userDeviceControl('en', 'control 60.5% value', ['someLevel'], '%n %s%u written', function (text) {
-            if (debug) console.log('userDeviceControl(someSwitch, 60.5%) returned: ' + text);
+            debug && console.log('userDeviceControl(someSwitch, 60.5%) returned: ' + text);
             expect(writtenValue).to.be.equal(60.5);
             expect(text).to.be.ok;
             expect(text.indexOf('some Level 60.5% written')).to.be.at.least(0);
@@ -259,7 +311,7 @@ describe('Commands: Test device control', function () {
 
     it('must control device with variable value -60', function (done) {
         simpleControl.userDeviceControl('en', 'control -60% value', ['someLevel'], '%n %s%u written', function (text) {
-            if (debug) console.log('userDeviceControl(someSwitch, -60%) returned: ' + text);
+            debug && console.log('userDeviceControl(someSwitch, -60%) returned: ' + text);
             expect(writtenValue).to.be.equal(-60);
             expect(text).to.be.ok;
             expect(text.indexOf('some Level -60% written')).to.be.at.least(0);
@@ -269,7 +321,7 @@ describe('Commands: Test device control', function () {
 
     it('must control device with variable value +60,6', function (done) {
         simpleControl.userDeviceControl('en', 'control +60,6% value', ['someLevel'], '%n %s%u written', function (text) {
-            if (debug) console.log('userDeviceControl(someSwitch, +60,6%) returned: ' + text);
+            debug && console.log('userDeviceControl(someSwitch, +60,6%) returned: ' + text);
             expect(writtenValue).to.be.equal(60.6);
             expect(text).to.be.ok;
             expect(text.indexOf('some Level 60.6% written')).to.be.at.least(0);
@@ -279,7 +331,7 @@ describe('Commands: Test device control', function () {
 
     it('must not return any ack text', function (done) {
         simpleControl.userDeviceControl('en', 'control 60.5% value', ['someSwitch'], '', function (text) {
-            if (debug) console.log('userDeviceControl(someSwitch, no ack) returned: ' + text);
+            debug && console.log('userDeviceControl(someSwitch, no ack) returned: ' + text);
             expect(text).to.be.not.ok;
             done();
         });
@@ -289,35 +341,35 @@ describe('Commands: Test device control', function () {
 describe('Commands: Test extract text', function () {
     it('Must return text except key words', function (done) {
         simpleControl.sendText('en', simpleControl.extractText('say to computer i will be back', 'say to computer I will be back', 'say [to] computer'), ['someSwitch'], 'Text: %s/Text: %s', function (text) {
-            if (debug) console.log('userText(say to computer I will be late, someSwitch) returned: ' + text);
+            debug && console.log('userText(say to computer I will be late, someSwitch) returned: ' + text);
             expect(text).to.be.equal('Text: I will be back');
             done();
         });
     });
     it('Must return value except key words', function (done) {
         simpleControl.sendText('en', simpleControl.extractText('say to computer active?', 'say to computer Active!', 'say [to] computer'), ['someSwitch'], 'Text: %s/Text: %s', function (text) {
-            if (debug) console.log('userText(say to computer Active!, someSwitch) returned: ' + text);
+            debug && console.log('userText(say to computer Active!, someSwitch) returned: ' + text);
             expect(text).to.be.equal('Text: active'); // someSwitch is boolean and active will be replaced with true
             done();
         });
     });
     it('Must return text except key words', function (done) {
         simpleControl.sendText('en', simpleControl.extractText('позвать гаража aндрей ужин готов!', 'позвать гаража Андрей ужин готов!', 'позвать [из] гаража'), ['someSwitch'], 'Text: %s/Text: %s', function (text) {
-            if (debug) console.log('userText(позвать гаража Андрей ужин готов!, someSwitch) returned: ' + text);
+            debug && console.log('userText(позвать гаража Андрей ужин готов!, someSwitch) returned: ' + text);
             expect(text).to.be.equal('Text: Андрей ужин готов!');
             done();
         });
     });
     it('Must return text except key words', function (done) {
         simpleControl.sendText('en', simpleControl.extractText('позвать из гаража aндрей ужин готов!', 'позвать из гаража Андрей ужин готов!', 'позвать [из] гаража'), ['someSwitch'], 'Text: %s/Text: %s', function (text) {
-            if (debug) console.log('userText(позвать из гаража Андрей ужин готов!, someSwitch) returned: ' + text);
+            debug && console.log('userText(позвать из гаража Андрей ужин готов!, someSwitch) returned: ' + text);
             expect(text).to.be.equal('Text: Андрей ужин готов!');
             done();
         });
     });
     it('Must return text except key words', function (done) {
         simpleControl.sendText('en', simpleControl.extractText('позвать из гаража aндрей ужин готов_', 'позвать из гаража Андрей ужин готов!', 'позвать из гаража'), ['someSwitch'], 'Text: %s/Text: %s', function (text) {
-            if (debug) console.log('userText(позвать из гаража Андрей ужин готов!, someSwitch) returned: ' + text);
+            debug && console.log('userText(позвать из гаража Андрей ужин готов!, someSwitch) returned: ' + text);
             expect(text).to.be.equal('Text: Андрей ужин готов!');
             done();
         });
