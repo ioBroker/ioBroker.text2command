a5c94d7c47aae39793c7c3dc6fb38a10b5b5104a
GermanBluefox <dogafox@gmail.com>
2023-02-21 12:23:33 +0100

    Correcting: https://github.com/ioBroker/ioBroker.text2command/issues/204

--- src/src/components/Layout.js
+++ src/src/components/Layout.js
@@ -414,7 +414,12 @@ class Layout extends PureComponent {
                 if (typeof rule.ack === 'object') {
                     rule.ack = rule.ack.default;
                 }
-                rule.args.forEach((arg, index) => {
+
+                if (rule.args && !Array.isArray(rule.args)) {
+                    rule.args = [];
+                }
+
+                rule.args && rule.args.forEach((arg, index) => {
                     if (typeof arg === 'object') {
                         rule.args[index] = arg.default;
                     }
